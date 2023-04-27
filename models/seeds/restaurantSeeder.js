c4c4e9cd37d37e1fee41261d12904d642e7690df
eunicebibi <agjay410@gmail.com>
2023-04-27 20:32:27 +0800

    modify Seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,18 +1,59 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../restaurant') // 載入model
-const restaurantList = require("../../restaurant.json").results
-
+const restaurantList = require('../../restaurant.json').results
+const User = require('../user')
 const db = require('../../config/mongoose')
 // db.on('error', () => {
 //   console.log('mongodb error!')
 // })
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
+  }]
+
 db.once('open', () => {
-  for (let i = 0; i < 8; i++) {
-    Restaurant.create(restaurantList)
-      .then(() => {
-        console.log("restaurantSeeder done!")
-        db.close()
-      })
-      .catch(err => console.log(err))
-  }
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER[0].password, salt))
+    .then(hash => User.create({
+      name: SEED_USER[0].name,
+      email: SEED_USER[0].email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      for (let i = 0; i < 3; i++) {
+        Restaurant.create({ ...restaurantList[i], userId })
+      }
+    })
+
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER[1].password, salt))
+    .then(hash => User.create({
+      name: SEED_USER[1].name,
+      email: SEED_USER[1].email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      for (let i = 3; i < 6; i++) {
+        Restaurant.create({ ...restaurantList[i], userId })
+      }
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
