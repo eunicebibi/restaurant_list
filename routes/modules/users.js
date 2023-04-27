cbe1d1957504cd7d39f258958a10295980908257
eunicebibi <agjay410@gmail.com>
2023-04-27 11:33:08 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -43,4 +43,12 @@ router.post('/register', (req, res) => {
   })
     .catch(err => console.log(err))
 })
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/users/login');
+  });
+});
+
 module.exports = router
